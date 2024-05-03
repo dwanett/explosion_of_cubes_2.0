88edc88fd6644203a810cbd73196c92acee64802
dwanett <Niki503@yandex.ru>
2024-05-03 22:11:44 +0300

    fix

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -1,15 +1,56 @@
 using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
-[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Renderer))]
+[RequireComponent(typeof(Collider))]
 public class Cube : MonoBehaviour
 {
+    [SerializeField] public Renderer _renderer;
+    private int _maxChanceSplit = 100;
+    private int _decreaceChance = 2;
+    
     public event Action ClikedCube;
-    private MaterialPropertyBlock _materialPropertyBlock;
+    
+    [field: SerializeField] public Collider Collider { get; private set; }
+    
+    public int ChanceSplit { get; private set; }
+    public int NumberGeneratedIterator { get; private set; }
+
+    private void Awake()
+    {
+        ChanceSplit = _maxChanceSplit;
+        NumberGeneratedIterator = 1;
+    }
     
     private void OnMouseUpAsButton()
     {
         ClikedCube?.Invoke();
         Destroy(gameObject);
+    }
+    
+    public void ReplaceColor(Color color)
+    {
+        _renderer.material.color = color;
+    }
+    
+    public void ReduceChanceSplit()
+    {
+        ChanceSplit /= _decreaceChance;
+    }
+    
+    public void ReplaceNumberGeneratedIterator(int numberGeneratedIterator)
+    {
+        NumberGeneratedIterator = numberGeneratedIterator;
+    }
+    
+    public void DownScaleCube(float DownScaleIndex)
+    {
+        transform.localScale /= DownScaleIndex;
+    }
+
+    public bool TrySplitCube()
+    {
+        return Random.Range(0, _maxChanceSplit + 1) < ChanceSplit;
     }
 }
--- Assets/Scripts/ExplodeCube.cs
+++ Assets/Scripts/ExplodeCube.cs
@@ -1,11 +1,10 @@
-
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ExplodeCube : MonoBehaviour
 {
     [SerializeField] private GenerateCube _generateCube;
-    [SerializeField] private Cube _controllerCube;
+    [SerializeField] private Cube _cube;
     [SerializeField] private float _forceExplode;
     [SerializeField] private float _radiusExplode;
     
@@ -21,15 +20,15 @@ public class ExplodeCube : MonoBehaviour
     
     private void Explode(int multiplierExplode)
     {
-        Vector3 explosionPos = _controllerCube.gameObject.transform.position;
+        Vector3 explosionPos = _cube.transform.position;
         float radius = _radiusExplode * multiplierExplode;
         
         List<Collider> colliders = new List<Collider>(Physics.OverlapSphere(explosionPos, radius));
-        colliders.Remove(_controllerCube.gameObject.GetComponent<Collider>());
+        colliders.Remove(_cube.Collider);
         
         foreach (Collider collider in colliders)
         {
-            Rigidbody rigidbody = collider.GetComponent<Rigidbody>();
+            Rigidbody rigidbody = collider.attachedRigidbody;
             
             if (rigidbody != null)
                 rigidbody.AddExplosionForce(_forceExplode * multiplierExplode, explosionPos, radius);
